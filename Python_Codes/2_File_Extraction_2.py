77447fe3f0d225c9989c794a303f7d1216f47239
Bastian-VM <bastian0VM@outlook.com>
2026-08-25 22:12:26 -0400

    Update 2_File_Extraction_2.py

--- Python_Codes/2_File_Extraction_2.py
+++ Python_Codes/2_File_Extraction_2.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 # ============================================================
 
 # Working folder setup
-folder = r"D:\Memoria_Last\Simulaciones_Xschem"
+folder = r"D:\Memoria_Last\Simulation_Xschem"
 py_folder = os.path.join(folder, "Files_PY")
 os.makedirs(py_folder, exist_ok=True)
 
