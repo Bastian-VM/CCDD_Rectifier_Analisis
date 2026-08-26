12503df3a495e895b2b326228ef331da0d5c1b22
Bastian-VM <bastian0VM@outlook.com>
2026-08-25 22:15:52 -0400

    Update 4-3_Sim_Cs_Comparison.py

--- Python_Codes/4-3_Sim_Cs_Comparison.py
+++ Python_Codes/4-3_Sim_Cs_Comparison.py
@@ -34,7 +34,7 @@ print("Sum of weights:", wPCE+wPin+wVout+wVRF+wArea+wSize)
 # -------------------------------
 # Working folder setup
 # -------------------------------
-folder = r"D:\Memoria_Last\Simulaciones_Xschem"
+folder = r"D:\Memoria_Last\Simulation_Xschem"
 py_folder = os.path.join(folder, "Files_PY")
 os.makedirs(py_folder, exist_ok=True)
 
